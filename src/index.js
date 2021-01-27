4e4833d54dbde3b78de431af5159d5a6fca1cec2
unknown <singh.avvi98@gmail.com>
2021-01-27 23:08:48 +1100

    .env set up and all relevant info transferred

--- src/index.js
+++ src/index.js
@@ -3,10 +3,8 @@ require('./db/mongoose');
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
 
-//SendGrid API Key = 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  
-
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(express.json()); //Setting This line means express will automatically parse incoming JSON to an object so we can access it in our request handlers.   
 app.use(userRouter);
