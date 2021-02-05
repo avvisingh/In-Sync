7563c93589b117c83aa7e645def9250d12b5c4ab
unknown <singh.avvi98@gmail.com>
2021-02-05 15:37:03 +1100

    Redirect on logout

--- client/src/Components/Logout.js
+++ client/src/Components/Logout.js
@@ -1,6 +1,9 @@
-import { useHistory } from "react-router-dom";
+import { useState } from "react";
+import { Redirect, useHistory } from "react-router-dom";
 
 const Logout = () => {
+  const [isLoggedOut, setIsLoggedOut] = useState(false);
+
   const handleClick = (e) => {
     e.preventDefault();
     fetch("http://localhost:8080/users/logout", {
@@ -9,11 +12,18 @@ const Logout = () => {
         "Content-Type": "application/x-www-form-urlencoded",
         Authorization: `Bearer ${localStorage.getItem("auth-token")}`,
       },
-    });
+    })
+      .then(() => {
+        setIsLoggedOut(true);
+      })
+      .catch((e) => {
+        console.log(e.message);
+      });
   };
   return (
     <div className="logout-button">
       <button onClick={handleClick}>Logout</button>
+      {isLoggedOut && <Redirect to="/login" />}
     </div>
   );
 };
