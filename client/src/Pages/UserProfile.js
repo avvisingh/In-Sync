7b10204158384546f5750c3ba0eecd1ab135a7ef
unknown <singh.avvi98@gmail.com>
2021-02-08 19:29:40 +1100

    user profile picture shows

--- client/src/Pages/UserProfile.js
+++ client/src/Pages/UserProfile.js
@@ -2,11 +2,21 @@ import "../Styles/UserProfile.css";
 import Logout from "../Components/Logout";
 
 const UserProfile = ({ user, title }) => {
+  let imgSource = `http://localhost:8080/users/${user._id}/avatar`;
+  // let avatar;
+  // fetch(`http://localhost:8080/users/${user._id}/avatar`)
+  //   .then((res) => {
+  //     const avatar = res;
+  //     console.log(res.json());
+  //   })
+  //   .catch((e) => console.error(e.message));
+
   return (
     <div className="profile-container">
       <h2>{title}</h2>
       <div className="name">{user.name}</div>
       <div className="age">{user.age}</div>
+      <img src={imgSource} className="avatar" />
       <Logout />
     </div>
   );
