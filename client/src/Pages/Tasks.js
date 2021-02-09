fec2be897ba4de45cd5a4c9469613a15b39c26f5
unknown <singh.avvi98@gmail.com>
2021-02-09 21:15:16 +1100

    tasks page styled

--- client/src/Pages/Tasks.js
+++ client/src/Pages/Tasks.js
@@ -1,17 +1,20 @@
 import { Link } from "react-router-dom";
+import "../Styles/Tasks.css";
 
 const Tasks = ({ tasks, title }) => {
   return (
-    <div className="task-list">
-      <h2>{title}</h2>
-      {tasks.map((task) => (
-        <div className="task-preview" key={task._id}>
-          <Link to={`/tasks/${task._id}`}>
-            <h4>{task.description}</h4>
-            <p>Task Has Been Completed: {task.completed.toString()}</p>
-          </Link>
-        </div>
-      ))}
+    <div className="containing">
+      <div className="task-list">
+        <h2>{title}</h2>
+        {tasks.map((task) => (
+          <div className="task-preview" key={task._id}>
+            <Link to={`/tasks/${task._id}`}>
+              <h4>{task.description}</h4>
+              <p>Task Has Been Completed: {task.completed.toString()}</p>
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
