21d7ed16e7ca586d6ca3273c0a2b58e784652791
unknown <singh.avvi98@gmail.com>
2021-02-09 20:08:57 +1100

    background added

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./Styles/App.css";
 import Navbar from "./Components/Navbar";
 import Home from "./Pages/Home";
 import Login from "./Pages/Login";
@@ -11,36 +12,34 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <div className="App">
-          <div className="content">
-            <Switch>
-              <Route exact path="/login">
-                <Login />
-              </Route>
-              <Route exact path="/newUser">
-                <NewUser />
-              </Route>
-              <Route exact path="/">
-                <Navbar />
-                <Home />
-              </Route>
-              <Route exact path="/tasks">
-                <Navbar />
-                <TaskList />
-              </Route>
-              <Route path="/tasks/:id">
-                <Navbar />
-                <TaskDetails />
-              </Route>
-              <Route path="/createtask">
-                <Navbar />
-                <CreateTask />
-              </Route>
-              {/* <Route exact path="/myprofile">
+        <div className="content">
+          <Switch>
+            <Route exact path="/login">
+              <Login />
+            </Route>
+            <Route exact path="/newUser">
+              <NewUser />
+            </Route>
+            <Route exact path="/">
+              <Navbar />
+              <Home />
+            </Route>
+            <Route exact path="/tasks">
+              <Navbar />
+              <TaskList />
+            </Route>
+            <Route path="/tasks/:id">
+              <Navbar />
+              <TaskDetails />
+            </Route>
+            <Route path="/createtask">
+              <Navbar />
+              <CreateTask />
+            </Route>
+            {/* <Route exact path="/myprofile">
                 <Home />
               </Route> */}
-            </Switch>
-          </div>
+          </Switch>
         </div>
       </Router>
     </div>
